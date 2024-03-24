512937fd2e2ab3f92e6afe92a2fa7e0c2f195492
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-24 13:41:19 +0300

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="mx-auto my-12 overflowed px-4 text-gray-1200 antialiased sm:my-32 md:my-[3px] md:mb-40 md:px-6 max-w-[1144px]">
+      <p className="text-2xl font-bold">Hello, world!</p>
+    </main>
+  );
+}
