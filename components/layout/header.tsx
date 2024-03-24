1b94d52110ca04b297a8d1b7e5ddf8a0925ba5fd
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-24 16:37:18 +0300

    feat: header and navbar

---
+++ components/layout/header.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import HeaderNav, { type HeaderItem } from './nav';
+
+const HEADER_ITEMS: HeaderItem[] = [
+  { label: 'Videos', href: '/' },
+  { label: 'Vault', href: '/about' },
+  {
+    label: 'Account',
+    href: '/settings',
+    children: [
+      { label: 'Settings', href: '/settings' },
+      { label: 'Logout', href: '/logout' },
+    ],
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="text-gray-1200 mx-auto flex max-w-[1144px] items-center justify-between px-4 py-12 antialiased md:mb-12 md:px-6">
+      <Link
+        href="/"
+        className="text-gray-1200 flex items-center gap-2 font-medium no-underline"
+      >
+        <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+      </Link>
+
+      <HeaderNav items={HEADER_ITEMS} />
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/layout/nav.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import Link from 'next/link';
+import { ChevronDown } from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+
+export interface HeaderItem {
+  label: string;
+  href: string;
+  children?: HeaderItem[];
+}
+
+interface HeaderItemProps {
+  items: HeaderItem[];
+}
+
+const HeaderNav = ({ items }: HeaderItemProps) => {
+  return (
+    <nav className="flex gap-4 lg:gap-6 xl:gap-8">
+      {items.map((item) => (
+        <div key={item.label} className="group relative">
+          {item.children ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <button className="inline-flex items-center gap-1 font-normal text-gray-600 no-underline transition-all">
+                  {item.label}
+                  <ChevronDown size={16} className="inline-block" />
+                </button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {item.children.map((child) => (
+                  <Link key={child.label} href={child.href}>
+                    <DropdownMenuItem className="group block cursor-pointer rounded-md p-2 text-gray-600 no-underline transition-all hover:bg-gray-100 hover:text-gray-800">
+                      {child.label}
+                    </DropdownMenuItem>
+                  </Link>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <Link
+              href={item.href}
+              className="font-normal text-gray-600 no-underline transition-all hover:text-gray-800"
+            >
+              {item.label}
+            </Link>
+          )}
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+export default HeaderNav;
